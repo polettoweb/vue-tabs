bb60178ca996469c9d8285512d1ea3330082028b
Marco Poletto <marco@momice.com>
2019-07-30 18:33:06 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,58 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Item v-for="item in items" :key="item.id" @clicked="onClickChild" :ref="item.id" :item="item"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
+  name: 'vue-tabs',
   components: {
-    HelloWorld
+    Item: () => import('./components/Item')
+  },
+  data() {
+    return {
+      ids: [],
+      items: [
+        {
+          id: 1,
+          title: "Title 1",
+          content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        },
+        {
+          id: 2,
+          title: "Title 2",
+          content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        },
+        {
+          id: 3,
+          title: "Title 3",
+          content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        }
+
+      ]
+    }
+  },
+  methods: {
+    listOfIds() {
+      this.items.map( item => {
+        return this.ids.push(item.id);
+      })
+    },
+    onClickChild(value) {
+      this.ids.map( item => {
+        if (item === value) {
+          return this.$refs[value][0]._data.active = ! this.$refs[value][0]._data.active
+        } else {
+          return this.$refs[item][0]._data.active = false
+        }
+      })
+    }
+  },
+  mounted() {
+    this.listOfIds();
   }
 }
 </script>
---
+++ src/components/Item.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h3>{{item.title}}</h3>
+        <p v-show="active">{{item.content}}</p>
+        <a @click="toggle(item.id)">{{ active ? 'Reade less' : 'Read more'}}</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "item",
+    props: {
+        item: Object
+    },
+    data() {
+        return {
+            active: false
+        }
+    },
+    methods: {
+        toggle(id) {
+            this.$emit("clicked", id)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
